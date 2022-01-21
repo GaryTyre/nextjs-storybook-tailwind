4fca3d94250952076dad851420a3f8b5a0bb024f
Gary Tyre <gary@foodsconnected.com>
2022-01-21 09:35:08 +0000

    top menu

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ import { Dialog, Menu, Transition } from '@headlessui/react'
 import {
   CogIcon,
   CollectionIcon,
-  HomeIcon,
   MenuAlt2Icon,
-  PhotographIcon,
   MapIcon,
   UserGroupIcon,
   ViewGridIcon,
@@ -14,7 +12,8 @@ import {
   CheckCircleIcon,
   CheckIcon
 } from '@heroicons/react/outline'
-import { SearchIcon } from '@heroicons/react/solid'
+import ExampleMenu from '../stories/Menu'
+import Breadcrumbs from '../stories/Breadcrumbs'
 
 const sidebarNavigation = [
   { name: 'Supplier List', href: '#', icon: CollectionIcon, current: false },
@@ -40,18 +39,12 @@ export default function Example() {
 
   return (
     <>
+      <ExampleMenu></ExampleMenu>
       <div className="h-full flex">
         {/* Narrow sidebar */}
         <div className="hidden w-28 bg-gray-700 overflow-y-auto md:block">
-          <div className="w-full py-4 flex flex-col items-center">
-            <div className="flex-shrink-0 flex items-center">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
-                alt="Workflow"
-              />
-            </div>
-            <div className="flex-1 mt-6 w-full px-2 space-y-1">
+          <div className="w-full py-2 flex flex-col items-center">
+            <div className="flex-1 w-full px-2 space-y-1">
               {sidebarNavigation.map((item) => (
                 <a
                   key={item.name}
@@ -168,7 +161,7 @@ export default function Example() {
         {/* Content area */}
         <div className="flex-1 flex flex-col overflow-hidden">
           <header className="w-full">
-            <div className="relative z-10 flex-shrink-0 h-16 bg-white border-b border-gray-200 shadow-sm flex">
+            <div className="relative z-10 flex-shrink-0 h-12 bg-white border-b border-gray-200 shadow-sm flex">
               <button
                 type="button"
                 className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 md:hidden"
@@ -177,70 +170,7 @@ export default function Example() {
                 <span className="sr-only">Open sidebar</span>
                 <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
               </button>
-              <div className="flex-1 flex justify-between px-4 sm:px-6">
-                <div className="flex-1 flex">
-                  <form className="w-full flex md:ml-0" action="#" method="GET">
-                    <label htmlFor="search-field" className="sr-only">
-                      Search all files
-                    </label>
-                    <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                        <SearchIcon className="flex-shrink-0 h-5 w-5" aria-hidden="true" />
-                      </div>
-                      <input
-                        name="search-field"
-                        id="search-field"
-                        className="h-full w-full border-transparent py-2 pl-8 pr-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 focus:border-transparent focus:placeholder-gray-400"
-                        placeholder="Search"
-                        type="search"
-                      />
-                    </div>
-                  </form>
-                </div>
-                <div className="ml-2 flex items-center space-x-4 sm:ml-6 sm:space-x-6">
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative flex-shrink-0">
-                    <div>
-                      <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-gray-700'
-                                )}
-                              >
-                                {item.name}
-                              </a>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-
-                </div>
-              </div>
+              <Breadcrumbs></Breadcrumbs>
             </div>
           </header>
 
